6c62eb9cb56795815ea2ad44b0be8bd01a1645d3
Jasdeep Singh Saini <jasaini07@gmail.com>
2026-08-01 12:43:29 +0530

    Time: 3 ms (12.28%), Space: 14.2 MB (58.93%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,17 +1,42 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
+        // int n = prices.size();//brute force tareeka solve krna ka 
+        // // jab STACK PADHLEGA TAB ISKA OPTIMAL SOLUTION SAMJ AYEAGA JO STACK USE KRKE HOTA HAI 
+        // for( int i =0;i<n;i++){
+        //     for( int j =i+1;j<n;j++){
+        //         if( prices[j]<=prices[i]){
+        //             prices[i]=prices[i]-prices[j];
+        //             break;
+        //         }
+        //     }
+
+        // }
+        // return prices;
+
+        //optimal solution is based on next smaller number concept lag rha h 
+        vector<int>ans(prices.size());
+        stack<int>s;
         int n = prices.size();
-        for( int i =0;i<n;i++){
-            for( int j =i+1;j<n;j++){
-                if( prices[j]<=prices[i]){
-                    prices[i]=prices[i]-prices[j];
-                    break;
-                }
+        for( int i = n-1 ; i>=0;i--){
+            while( s.size() > 0 && s.top() >  prices[i]){
+                s.pop();
             }
 
+            if(s.empty()){
+                ans[i] = prices[i];
+            }
+
+            else{
+                ans[i]  = prices[i] - s.top();
+            } 
+
+            s.push(prices[i]);
         }
-        return prices;
+
+        return ans;
+
+
         
     }
 };
