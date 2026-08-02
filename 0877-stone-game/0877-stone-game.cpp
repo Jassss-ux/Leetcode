3e140d902fdf106b23c111aafee318f74c463f2c
Jasdeep Singh Saini <jasaini07@gmail.com>
2026-08-02 18:21:04 +0530

    Time: 0 ms (100%), Space: 10.3 MB (87.75%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        return true;  //lenght even h piles ki hameshaa first player hi jeeteagaa observed from constartaints
         
         
     }
