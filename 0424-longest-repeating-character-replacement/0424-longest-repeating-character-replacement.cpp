c04cccb09d89bdbc44d1bac4900939ddc685ce31
Jasdeep Singh Saini <jasaini07@gmail.com>
2026-08-15 08:03:46 +0530

    Time: 3 ms (67.47%), Space: 11 MB (42.12%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,31 +1,61 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
+
+        // 'left' and 'right' represent the boundaries of our sliding window
         int left = 0;
         int right = 0;
+
+        // Stores the maximum length of a valid window found so far
         int maxlen = 0;
+
+        // Stores the highest frequency of any single character
+        // present in the current/previous windows
         int maxfreq = 0;
 
-        unordered_map<char, int> freq;
+        // Frequency array for 26 uppercase English letters
+        // hash[0] = frequency of 'A'
+        // hash[1] = frequency of 'B'
+        // ...
+        // hash[25] = frequency of 'Z'
+        vector<int> hash(26, 0);
+
         int n = s.length();
+
+        // Expand the sliding window using 'right'
         while (right < n) {
-            freq[s[right]]++;
-            maxfreq = max(maxfreq, freq[s[right]]);
 
-            while ((right - left + 1) - maxfreq > k) {
-                freq[s[left]]--;
+            // Increase the frequency of the current character
+            // Example: if s[right] = 'A', then hash[0]++
+            hash[s[right] - 'A']++;
 
-                if (freq[s[left]] == 0) {
-                    freq.erase(s[left]);
-                }
+            // Update the maximum frequency in the window
+            // We need this to know which character occurs the most
+            maxfreq = max(maxfreq, hash[s[right] - 'A']);
 
+            // Number of characters that need to be replaced is:
+            // Window size - frequency of most frequent character
+            //
+            // If replacements required > k, the current window
+            // is invalid, so we shrink it from the left.
+            if ((right - left + 1) - maxfreq > k) {
+
+                // Remove the leftmost character from the window
+                hash[s[left] - 'A']--;
+
+                // Move left forward to make the window smaller
                 left++;
             }
 
-            maxlen = max(maxlen, (right - left + 1));
+            // The current window is valid, so update the maximum length
+            maxlen = max(maxlen, right - left + 1);
 
+            // Move right forward to include the next character
             right++;
         }
+
+        // Return the longest substring that can be made
+        // of the same character using at most k replacements
         return maxlen;
     }
 };
