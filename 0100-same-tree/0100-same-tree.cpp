85418703181dee1443238eb52c54ba7fbae03836
Jasdeep Singh Saini <jasaini07@gmail.com>
2026-08-30 11:07:57 +0530

    Time: 0 ms (100%), Space: 13 MB (13.24%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -10,14 +10,30 @@
  * right(right) {}
  * };
  */
+
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
+
+        // If either of the nodes is NULL,
+        // then both should be NULL for the trees to be same.
+        // p == q will return:
+        // true  -> if both are NULL
+        // false -> if only one is NULL
         if (p == NULL || q == NULL) {
             return p == q;
         }
+
+        // Recursively check whether the left subtrees are same
         bool leftsubstreesame = isSameTree(p->left, q->left);
+
+        // Recursively check whether the right subtrees are same
         bool rightsubstreesame = isSameTree(p->right, q->right);
+
+        // For two trees to be same:
+        // 1. Left subtrees must be same
+        // 2. Right subtrees must be same
+        // 3. Current node values must be same
         return leftsubstreesame && rightsubstreesame && (p->val == q->val);
     }
 };
